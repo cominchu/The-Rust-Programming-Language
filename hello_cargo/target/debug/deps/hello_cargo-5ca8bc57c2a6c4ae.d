ed9000495d2be93220c4abedb22c60882eaa9107
cominchu <cominchu07@gmail.com>
2020-04-26 19:29:56 +0900

    done chapter 1

---
+++ hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae.d
@@ -0,0 +1,5 @@
+/Users/fukui/Documents/program/rust/get_started/hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae: src/main.rs
+
+/Users/fukui/Documents/program/rust/get_started/hello_cargo/target/debug/deps/hello_cargo-5ca8bc57c2a6c4ae.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/fukui/Documents/program/rust/get_started/hello_cargo/target/debug/hello_cargo: /Users/fukui/Documents/program/rust/get_started/hello_cargo/src/main.rs
